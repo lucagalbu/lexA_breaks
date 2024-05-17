7e6474cd6d1a7f1ec5e40dfbca5072a56925ce4b
lucagalbu <lucagalbu@gmail.com>
2024-05-17 22:09:02 +0200

    Added constants

---
+++ src/main/scala/me/lucagalbusera/simplebreaks/Constants.scala
@@ -0,0 +1,7 @@
+package me.lucagalbusera.simplebreaks
+
+val LexaDecay: Double = 1.5
+val LexaSteadyState: Double = 200.0
+val LexaUnbinding: Double = 3.0
+val ProteinCreation: Double = 20.0
+val RecaSlope: Double = -0.0278
